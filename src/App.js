58436b0c4a2eeae65c1d89ac57845e1daae3984b
Darius Burgan <darius@dariusburgan.com>
2018-09-02 22:57:51 -0400

    Refactor route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
-import Header from "./components/header";
-import GoogleMap from "./components/googlemap";
 import { Route } from "react-router-dom";
+import Homepage from "./components/homepage";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route
-          exact
-          path="/"
-          render={() => {
-            return (
-              <React.Fragment>
-                <Header />
-                <GoogleMap />
-              </React.Fragment>
-            );
-          }}
-        />
+        <Route exact path="/" component={Homepage} />
       </div>
     );
   }

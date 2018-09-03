212085885f46dd311ae37f707541c80fcfabf09b
Darius Burgan <darius@dariusburgan.com>
2018-09-02 23:41:39 -0400

    Add click event to hamburger menu

--- src/components/header.js
+++ src/components/header.js
@@ -3,18 +3,16 @@ import React, { Component } from "react";
 export default class header extends Component {
   render() {
     return (
-      <React.Fragment>
-        <header className="navbar navbar-dark bg-dark">
-          <nav>
-            <div className="hamburger-menu">
-              <div />
-              <div />
-              <div />
-            </div>
-          </nav>
-          <h1 className="navbar-brand">Map App</h1>
-        </header>
-      </React.Fragment>
+      <header className="navbar navbar-dark bg-dark">
+        <nav>
+          <div className="hamburger-menu" onClick={this.props.onMenuClick}>
+            <div />
+            <div />
+            <div />
+          </div>
+        </nav>
+        <h1 className="navbar-brand">Map App</h1>
+      </header>
     );
   }
 }

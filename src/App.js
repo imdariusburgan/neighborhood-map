f6240a0ea043897ff5043a8c9ec471abff295d0c
Darius Burgan <darius@dariusburgan.com>
2018-09-02 20:53:35 -0400

    Put together homepage route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/header";
+import GoogleMap from "./components/googlemap";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return (
+              <React.Fragment>
+                <Header />
+                <GoogleMap />
+              </React.Fragment>
+            );
+          }}
+        />
       </div>
     );
   }

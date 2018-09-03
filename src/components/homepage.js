69a7e0e5232ca95b75aaf4b56390e26379ebc3a2
Darius Burgan <darius@dariusburgan.com>
2018-09-02 20:52:15 -0400

    Create component for homepage

---
+++ src/components/homepage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import GoogleMaps from "./components/googlemap";
+import Header from "./components/header";
+
+export default class homepage extends Component {
+  state = {
+      filteredListOpen = false
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Hi</h1>
+      </div>
+    );
+  }
+}
